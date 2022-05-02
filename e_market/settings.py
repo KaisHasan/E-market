265eee3c4ab9e4da5c3a56e1b3a71f10b7fe1c21
Kais Hasan <kais.hasan314@gmail.com>
2022-05-03 01:46:40 +0300

    Fixed issues in deployment

--- e_market/settings.py
+++ e_market/settings.py
@@ -118,7 +118,7 @@ WSGI_APPLICATION = 'e_market.wsgi.application'
 
 import dj_database_url
 DATABASES = dict()
-DATABASES['default'] = dj_database_url.config(env, conn_max_age=600)
+DATABASES['default'] = dj_database_url.config(conn_max_age=600)
 
 
 # Password validation
