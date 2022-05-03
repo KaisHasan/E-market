a6e718d1c7fc0d5717984382da6e0547d5aea2b6
Kais Hasan <kais.hasan314@gmail.com>
2022-05-03 11:12:52 +0300

    '.'

--- e_market/settings.py
+++ e_market/settings.py
@@ -30,7 +30,7 @@ environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
 SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = env('DEBUG')
+DEBUG = True # env('DEBUG')
 
 ALLOWED_HOSTS = [
     'local',
