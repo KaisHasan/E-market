3ee19617ed1435f951f9df7b317123b9631c686e
Kais Hasan <kais.hasan314@gmail.com>
2022-05-03 11:17:58 +0300

    '.'

--- e_market/settings.py
+++ e_market/settings.py
@@ -97,7 +97,7 @@ ROOT_URLCONF = 'e_market.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
