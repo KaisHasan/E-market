ce62dd4bf5e58b654b3176d17d1b48e37c457cee
Kais Hasan <kais.hasan314@gmail.com>
2022-05-22 16:48:50 +0300

    Added the number of items in the cart at the nav

--- accounts/models.py
+++ accounts/models.py
@@ -1,6 +1,13 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
+from django.db.models import Sum
 
 # Create your models here.
 class CustomUser(AbstractUser):
-    pass
+    def get_num_of_items_in_cart(self):
+        num = self.orders.aggregate(
+            n=Sum('num_items')
+        )['n']
+        if num > 9:
+            num = '+9'
+        return num
