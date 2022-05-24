79d04e23c3ddd9d3cf1c4d9db2f7f9de3e013e66
Kais Hasan <kais.hasan314@gmail.com>
2022-05-24 16:04:16 +0300

    Fix env issue

--- e_market/settings.py
+++ e_market/settings.py
@@ -33,16 +33,16 @@ if env == 'production':
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
+# environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = env('SECRET_KEY')
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = env('DEBUG')
+DEBUG = os.environ.get('DEBUG')
 
 ALLOWED_HOSTS = [
     'e-market314-v2.herokuapp.com', 'localhost', '127.0.0.1'
